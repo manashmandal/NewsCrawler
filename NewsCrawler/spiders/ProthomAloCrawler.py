15f97f1167acc472204ddfc747ca320b1b17976a
Manash Kumar Mandal <manashmndl@gmail.com>
2016-11-26 03:28:24 +0600

    gets breadcrumb and image captions

--- NewsCrawler/spiders/ProthomAloCrawler.py
+++ NewsCrawler/spiders/ProthomAloCrawler.py
@@ -67,15 +67,36 @@ class ProthomAloSpider(scrapy.Spider):
 
     def parseNews(self, response):
         self.logger.info("TRYING")
+        self.id += 1
 
         # Retreiving news item
         news_item = response.meta['news_item']
+
+        # Currently detects one image
+        news_item['images'] = self.get_image_url(response.xpath("//img/@src").extract())
+
+        # Currently gets one caption
+        news_item['image_captions'] = response.xpath("//div[@itemprop='articleBody']//span/text()").extract_first()
+
+        # Getting the breadcrumb
+        news_item['breadcrumb'] = response.xpath("//div[@class='breadcrumb']/ul/li/a/strong/text()").extract()
+
+        # Getting the article content
+        news_item['article'] = ''.join([para.strip() for para in response.xpath("//div[@itemprop='articleBody']//p/text()").extract()]
+
+        # Applying NLP from newspaper package [WARNING: slows the overall process]
+        article = Article(url=news_item['url'])
+        article.download()
+        article.parse()
+        article.nlp()
+        news_item['generated_summary'] = article.summary
+        news_item['generated_keywords'] = article.keywords
 
         yield {
             'title' : news_item['title'],
             'url' : news_item['url'],
             'reporter' : news_item['reporter'],
-            'location' : news_item['location']
+            'location' : news_item['news_location']
         }
     
     def get_location(self, inp):
@@ -83,3 +104,8 @@ class ProthomAloSpider(scrapy.Spider):
         if (len(inp_list) < 2):
             return None
         return inp_list[-1].strip()
+
+    def get_image_url(self, inp):
+        for image_url in inp:
+            if 'contents' and 'uploads' in image_url.split('/'):
+                return self.main_url + image_url
